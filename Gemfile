197219300bca72ec1611b50ce66e930e3176069f
pedro_bras <pedro.c.bras@gmail.com>
2019-08-23 15:35:53 +0200

    sets up unit and feature testing environment

--- Gemfile
+++ Gemfile
@@ -13,7 +13,12 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'cucumber-rails', require: false
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
+  gem 'pry-byebug'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,16 @@ Bundler.require(*Rails.groups)
 
 module UnterRails
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |generate|
+    generate.helper false
+    generate.assets false
+    generate.view_specs false
+    generate.helper_specs false
+    generate.routing_specs false
+    generate.controller_specs false
+    generate.system_tests false
+  end
   end
 end
